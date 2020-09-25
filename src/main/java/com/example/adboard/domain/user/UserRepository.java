c22846d267c52850ca2f45c30bf196531ac3b10a
leks <kurbakov.av@gmail.com>
2020-09-25 23:56:14 +0300

    add: UserRepository

---
+++ src/main/java/com/example/adboard/domain/user/UserRepository.java
@@ -0,0 +1,19 @@
+package com.example.adboard.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByUsernameOrEmail(String username, String email);
+
+    Optional<User> findByPassword_Reset_ResetCode(String code);
+
+    List<User> findAllByActivation_ActiveFalse();
+}
