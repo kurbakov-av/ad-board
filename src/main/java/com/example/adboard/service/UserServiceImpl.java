a70385085dc91babd4865c85e1ed70da6fbf444e
leks <kurbakov.av@gmail.com>
2020-09-27 21:35:06 +0300

    future: User Registration

--- src/main/java/com/example/adboard/service/UserServiceImpl.java
+++ src/main/java/com/example/adboard/service/UserServiceImpl.java
@@ -1,12 +1,14 @@
 package com.example.adboard.service;
 
-import com.example.adboard.domain.user.User;
-import com.example.adboard.domain.user.UserRepository;
+import com.example.adboard.domain.user.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.example.adboard.web.form.RegisterForm;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Set;
 
@@ -20,13 +22,27 @@ public class UserServiceImpl implements UserService {
 
 
     @Override
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @PreAuthorize("isAnonymous()")
     public void register(RegisterForm form) {
-
+        register(form, Set.of(Role.USER));
     }
 
     @Override
+    @Transactional
+    @PreAuthorize("hasRole('ADMIN')")
     public void register(RegisterForm form, Set<GrantedAuthority> authorities) {
+        Password password = new Password();
+        password.setValue(passwordEncoder.encode(form.getPassword()));
 
+        User user = new User();
+        user.setUsername(form.getUsername());
+        user.setPassword(password);
+        user.setEmail(form.getEmail());
+        user.setAuthorities(Set.of(Role.USER));
+        user.setContactCard(new ContactCard(form.getUsername(), null, null));
+
+        userRepository.save(user);
     }
 
     @Override
