eb15f2f8397b7ec627fd411ac7eae3a2b2ddbc57
leks <kurbakov.av@gmail.com>
2020-09-30 23:23:28 +0300

    add: get current user self complaints

--- src/main/java/com/example/adboard/service/ComplaintManagementImpl.java
+++ src/main/java/com/example/adboard/service/ComplaintManagementImpl.java
@@ -1,20 +1,29 @@
 package com.example.adboard.service;
 
 import com.example.adboard.domain.complaint.Complaint;
+import com.example.adboard.domain.complaint.ComplaintRepository;
 import com.example.adboard.domain.user.User;
 import com.example.adboard.web.form.ComplaintForm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class ComplaintManagementImpl implements ComplaintManagement {
 
+    private final ComplaintRepository complaintRepository;
+
+    private final UserService userService;
+
     @Override
+    @Transactional
+    @PreAuthorize("hasRole('USER')")
     public Page<Complaint> getCurrentUserComplaints(Pageable pageable) {
-        return null;
+        return complaintRepository.findByAuthor(userService.getCurrentUser(), pageable);
     }
 
     @Override
