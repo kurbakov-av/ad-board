c17d12c6fdc81ae7c22c110711d76ceec48736ad
leks <kurbakov.av@gmail.com>
2020-10-01 22:34:56 +0300

    add: send activation email code

--- src/main/java/com/example/adboard/service/notificate/EmailNotificationServiceImpl.java
+++ src/main/java/com/example/adboard/service/notificate/EmailNotificationServiceImpl.java
@@ -1,15 +1,26 @@
 package com.example.adboard.service.notificate;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 @RequiredArgsConstructor
 public class EmailNotificationServiceImpl implements EmailNotificationService {
 
+    private final MailSender mailSender;
+
     @Override
     public void sendActivationEmailCode(String email, String code) {
-
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email);
+        message.setSubject("Activation email");
+        message.setText(code);
+        message.setSentDate(new Date());
+        mailSender.send(message);
     }
 
     @Override
