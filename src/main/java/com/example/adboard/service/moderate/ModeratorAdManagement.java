6192cb80b5e84880f8ca0c0ec5da312c8c352d5e
leks <kurbakov.av@gmail.com>
2020-10-01 22:10:05 +0300

    add: publish ad

--- src/main/java/com/example/adboard/service/moderate/ModeratorAdManagement.java
+++ src/main/java/com/example/adboard/service/moderate/ModeratorAdManagement.java
@@ -1,16 +1,29 @@
 package com.example.adboard.service.moderate;
 
+import com.example.adboard.domain.Status;
 import com.example.adboard.domain.ad.Ad;
+import com.example.adboard.domain.ad.AdRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
 @Service
 @RequiredArgsConstructor
 public class ModeratorAdManagement implements ModeratorManagement<Ad> {
 
-    @Override
-    public void publish(Ad entity) {
+    private final AdRepository adRepository;
 
+    @Override
+    @Transactional
+    @PreAuthorize("hasRole('MODERATOR')")
+    public void publish(Ad entity) {
+        Assert.state(!entity.getAuthor().isBlocked(), "User blocked");
+        Assert.state(entity.getStatus().isModeration(), "Ad not being moderated");
+
+        entity.setStatus(Status.PUBLISHED);
+        adRepository.save(entity);
     }
 
     @Override
