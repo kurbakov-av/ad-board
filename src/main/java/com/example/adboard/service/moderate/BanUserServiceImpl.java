e1f479bd398325581a5a62252f487b6837ba3cce
leks <kurbakov.av@gmail.com>
2020-10-01 22:46:14 +0300

    add: ban user

--- src/main/java/com/example/adboard/service/moderate/BanUserServiceImpl.java
+++ src/main/java/com/example/adboard/service/moderate/BanUserServiceImpl.java
@@ -1,16 +1,44 @@
 package com.example.adboard.service.moderate;
 
 import com.example.adboard.domain.user.User;
+import com.example.adboard.domain.user.ban.UserBan;
+import com.example.adboard.domain.user.ban.UserBanRepository;
+import com.example.adboard.service.UserService;
 import com.example.adboard.web.form.UserBanForm;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
 @Service
 @RequiredArgsConstructor
 public class BanUserServiceImpl implements BanUserService {
 
-    @Override
-    public void ban(User user, UserBanForm form) {
+    private final UserBanRepository userBanRepository;
 
+    private final UserService userService;
+
+    @Override
+    @Transactional
+    @PreAuthorize("hasRole('MODERATOR')")
+    public void ban(User user, UserBanForm form) {
+        Assert.state(!user.isBlocked(), "User already blocked");
+
+        UserBan userBlock = new UserBan();
+        userBlock.setAuthor(userService.getCurrentUser());
+        userBlock.setReason(form.getReason());
+        userBlock.setStartAt(form.getStartAt());
+        userBlock.setTargetUser(user);
+        userBlock.setEndAt(form.getEndAt());
+
+        if (Boolean.TRUE.equals(form.getPermanent())) {
+            userBlock.setPermanent(true);
+        }
+
+        userBanRepository.save(userBlock);
+
+        user.setBlocked(true);
+        userService.save(user);
     }
 }
