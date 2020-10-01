5bd1f4087d58aa2254bc4c64478e6132c79909f0
leks <kurbakov.av@gmail.com>
2020-10-01 22:24:48 +0300

    add: update user avatar

--- src/main/java/com/example/adboard/service/AccountManagementImpl.java
+++ src/main/java/com/example/adboard/service/AccountManagementImpl.java
@@ -1,17 +1,26 @@
 package com.example.adboard.service;
 
 import com.example.adboard.domain.image.Image;
+import com.example.adboard.domain.user.User;
 import com.example.adboard.web.form.AccountForm;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class AccountManagementImpl implements AccountManagement {
 
-    @Override
-    public void updateAvatar(Image image) {
+    private final UserService userService;
 
+    @Override
+    @Transactional
+    @PreAuthorize("hasRole('USER')")
+    public void updateAvatar(Image image) {
+        User currentUser = userService.getCurrentUser();
+        currentUser.setAvatar(image);
+        userService.save(currentUser);
     }
 
     @Override
