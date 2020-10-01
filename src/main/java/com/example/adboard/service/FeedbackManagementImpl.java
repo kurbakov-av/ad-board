71acdff2f1fc92de39aa177e445fa451528e2838
leks <kurbakov.av@gmail.com>
2020-10-01 21:55:22 +0300

    add: get user feedbacks

--- src/main/java/com/example/adboard/service/FeedbackManagementImpl.java
+++ src/main/java/com/example/adboard/service/FeedbackManagementImpl.java
@@ -2,19 +2,28 @@ package com.example.adboard.service;
 
 import com.example.adboard.domain.ad.Ad;
 import com.example.adboard.domain.feedback.Feedback;
+import com.example.adboard.domain.feedback.FeedbackRepository;
 import com.example.adboard.web.form.FeedbackForm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class FeedbackManagementImpl implements FeedbackManagement {
 
+    private final FeedbackRepository feedbackRepository;
+
+    private final UserService userService;
+
     @Override
+    @Transactional
+    @PreAuthorize("hasRole('USER')")
     public Page<Feedback> getCurrentUserFeedback(Pageable pageable) {
-        return null;
+        return feedbackRepository.findAllByAuthor(userService.getCurrentUser(), pageable);
     }
 
     @Override
