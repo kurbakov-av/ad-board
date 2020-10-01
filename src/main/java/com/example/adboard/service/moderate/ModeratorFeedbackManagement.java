e78de7941aeabe2389bd9eba350abd6795fd9792
leks <kurbakov.av@gmail.com>
2020-10-01 22:15:47 +0300

    add: publish feedback

--- src/main/java/com/example/adboard/service/moderate/ModeratorFeedbackManagement.java
+++ src/main/java/com/example/adboard/service/moderate/ModeratorFeedbackManagement.java
@@ -1,16 +1,29 @@
 package com.example.adboard.service.moderate;
 
+import com.example.adboard.domain.Status;
 import com.example.adboard.domain.feedback.Feedback;
+import com.example.adboard.domain.feedback.FeedbackRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
 @Service
 @RequiredArgsConstructor
 public class ModeratorFeedbackManagement implements ModeratorManagement<Feedback> {
 
-    @Override
-    public void publish(Feedback entity) {
+    private final FeedbackRepository feedbackRepository;
 
+    @Override
+    @Transactional
+    @PreAuthorize("hasRole('MODERATOR')")
+    public void publish(Feedback entity) {
+        Assert.state(!entity.getAuthor().isBlocked(), "User blocked");
+        Assert.state(entity.getStatus().isModeration(), "Feedback not being moderated");
+
+        entity.setStatus(Status.PUBLISHED);
+        feedbackRepository.save(entity);
     }
 
     @Override
