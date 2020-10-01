f327218e11fdf7e67892ea168c37ce8c4807ec93
leks <kurbakov.av@gmail.com>
2020-10-01 22:12:30 +0300

    add: publish complaint

--- src/main/java/com/example/adboard/service/moderate/ModeratorComplaintManagement.java
+++ src/main/java/com/example/adboard/service/moderate/ModeratorComplaintManagement.java
@@ -1,16 +1,29 @@
 package com.example.adboard.service.moderate;
 
+import com.example.adboard.domain.Status;
 import com.example.adboard.domain.complaint.Complaint;
+import com.example.adboard.domain.complaint.ComplaintRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
 @Service
 @RequiredArgsConstructor
 public class ModeratorComplaintManagement implements ModeratorManagement<Complaint> {
 
-    @Override
-    public void publish(Complaint entity) {
+    private final ComplaintRepository complaintRepository;
 
+    @Override
+    @Transactional
+    @PreAuthorize("hasRole('MODERATOR')")
+    public void publish(Complaint entity) {
+        Assert.state(!entity.getAuthor().isBlocked(), "User blocked");
+        Assert.state(entity.getStatus().isModeration(), "Complaint not being moderated");
+
+        entity.setStatus(Status.PUBLISHED);
+        complaintRepository.save(entity);
     }
 
     @Override
