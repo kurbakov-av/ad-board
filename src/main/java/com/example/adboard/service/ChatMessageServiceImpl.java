683369081f818d7973b59ae638921fb696009b31
leks <kurbakov.av@gmail.com>
2020-10-03 21:01:47 +0300

    add: create message to chat

--- src/main/java/com/example/adboard/service/ChatMessageServiceImpl.java
+++ src/main/java/com/example/adboard/service/ChatMessageServiceImpl.java
@@ -2,18 +2,28 @@ package com.example.adboard.service;
 
 import com.example.adboard.domain.chat.Chat;
 import com.example.adboard.domain.chat.ChatMessage;
+import com.example.adboard.domain.chat.ChatMessageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class ChatMessageServiceImpl implements ChatMessageService {
 
-    @Override
-    public void create(Chat chat, String text) {
+    private final ChatMessageRepository chatMessageRepository;
 
+    @Override
+    @Transactional
+    @PreAuthorize("hasRole('USER')")
+    public void create(Chat chat, String text) {
+        ChatMessage message = new ChatMessage();
+        message.setText(text);
+        message.setChat(chat);
+        chatMessageRepository.save(message);
     }
 
     @Override
