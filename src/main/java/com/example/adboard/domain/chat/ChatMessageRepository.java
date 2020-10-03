d029173a349739a2fbe172c74bea98be64d8fc07
leks <kurbakov.av@gmail.com>
2020-10-03 20:55:42 +0300

    define: ChatMessageRepository

---
+++ src/main/java/com/example/adboard/domain/chat/ChatMessageRepository.java
@@ -0,0 +1,12 @@
+package com.example.adboard.domain.chat;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+
+    Page<ChatMessage> findAllByChat(Chat chat, Pageable pageable);
+}
